ce1b721ec55a18e4cab5f8e7e9e00714b5cada47
ahmedrooshdii <ahmedrooshdii@gmail.com>
2023-03-20 03:26:26 +0200

    Solving example (2) of homeworf (1).

---
+++ [Lecture 3] - C Basics/Homework 1/EX2/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: ٢٩‏/٠١‏/٢٠٢٣
+ *      Author: ahmed
+ */
+#include <stdio.h>
+int main(){
+	int x;
+	printf("Enter a integer: ");
+	fflush(stdout);
+	scanf("%d",&x);
+	printf("You entered: %d",x);
+	return 0;
+}
